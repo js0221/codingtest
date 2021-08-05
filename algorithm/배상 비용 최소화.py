1a946b59809284bceede08b6b40aced838fa468c
Jooseop Kim <65662520+jooseop@users.noreply.github.com>
2021-08-05 18:44:13 +0900

    Update 배상 비용 최소화.py

--- algorithm/배상 비용 최소화.py
+++ algorithm/배상 비용 최소화.py
@@ -1,3 +1,4 @@
+# sol1
 from heapq import _heapify_max, _heappop_max
     def solution(no, works):
     if no >= sum(works): # 일의 총 시간 합이, no보다 작으면 0으로 만들 수 있다.
@@ -10,3 +11,21 @@ from heapq import _heapify_max, _heappop_max
         works.append(max_works)
 
     return sum([i ** 2 for i in works])
+
+
+# sol2
+from heapq import heapify, heapreplace
+def solution(no, works):
+    if no > sum(works):
+        return 0
+    
+    work = [-i for i in works]
+    heapify(work)
+    
+    for _ in range(no):
+        # heapreplace : 팝 후 푸시
+        # heappushpop : 푸시 후 팝
+        # 마이너스로 값을 넣었기때문에 가장 작은 값에 +1을 해주면 절댓값이 줄어듬
+        heapreplace(work, work[0] + 1)
+        
+    return sum([i ** 2 for i in work])

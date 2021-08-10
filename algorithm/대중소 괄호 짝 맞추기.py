43c8acaeef50e3d9b76d810160446e8da1da640e
Jooseop Kim <65662520+jooseop@users.noreply.github.com>
2021-08-10 10:55:11 +0900

    Create 대중소 괄호 짝 맞추기.py

---
+++ algorithm/대중소 괄호 짝 맞추기.py
@@ -0,0 +1,30 @@
+# solution 1
+def solution(s):
+    stack = []
+    
+    for c in s:
+        if stack:
+            if stack[-1] == '{' and c == '}':
+                stack.pop()
+            elif stack[-1] == '(' and c == ')':
+                stack.pop()
+            elif stack[-1] == '[' and c == ']':
+                stack.pop()
+            else:
+                stack.append(c)
+        else:
+            stack.append(c)
+    return bool(stack == [])
+    
+    
+# solution 2
+def solution(s):
+    stack = []
+
+    for c in s:
+        if stack and 0 < ord(c) - ord(stack[-1]) <= 2:
+            stack.pop()
+        else:
+            stack.append(c)
+
+    return True if len(stack) == 0 else False

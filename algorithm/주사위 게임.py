285d0bb476a45cde3daa03af4554315ede5f5ec3
Jooseop Kim <65662520+jooseop@users.noreply.github.com>
2021-08-11 18:39:06 +0900

    Update 주사위 게임.py

--- algorithm/주사위 게임.py
+++ algorithm/주사위 게임.py
@@ -31,3 +31,11 @@ def solution(monster, S1, S2, S3):
     mc = [lc.count(m) for m in monster]
 
     return int((len(lc) - sum(mc)) / len(lc) * 1000)
+
+# solution 3
+from itertools import product
+
+def solution(monster, S1, S2, S3):
+    p = product(range(S1), range(S2), range(S3))
+    case = len([x for x in p if sum(x) + 4 not in monster])
+    return int(case / (S1 * S2 * S3) * 1000)

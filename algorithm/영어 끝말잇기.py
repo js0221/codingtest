1a1a59466ad95f2052f872a9c442617e9b347edd
Jooseop Kim <65662520+jooseop@users.noreply.github.com>
2021-08-15 23:59:38 +0900

    Update 영어 끝말잇기.py

--- algorithm/영어 끝말잇기.py
+++ algorithm/영어 끝말잇기.py
@@ -1,3 +1,4 @@
+# solution 1
 def solution(n, words):
     word = words[0]
     check = {words[0]}
@@ -11,3 +12,10 @@ def solution(n, words):
     else:
         return 0, 0
     
+# solution 2
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i][0] != words[i-1][-1] or words[i] in words[:i] :
+            return [(i%n)+1, (i//n)+1]
+    else:
+        return [0,0]

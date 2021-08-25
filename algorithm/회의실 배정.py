5339bc58c6d4c4e7d64e666200c210c86ac01c5e
Jooseop Kim <65662520+jooseop@users.noreply.github.com>
2021-08-25 23:33:26 +0900

    Update 회의실 배정.py

--- algorithm/회의실 배정.py
+++ algorithm/회의실 배정.py
@@ -7,3 +7,17 @@ def solution(arr):
             answer += 1
             end = e
     return answer
+
+
+# 백준 solution
+n = int(input())
+time = [tuple(map(int, input().split())) for _ in range(n)]
+time.sort(key=lambda x : (x[1], x[0]))
+answer = 0
+
+prev_finish = 0
+for start, finish in time:
+    if prev_finish > start:
+        continue
+    prev_finish = finish
+    answer += 1
